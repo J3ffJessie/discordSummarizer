e03c8a696cec7810740cea310c806152b385048d
J3ffJessie <jejess6125@gmail.com>
2026-02-16 20:21:26 -0600

    Update gather.js

--- src/services/gather.js
+++ src/services/gather.js
@@ -1,31 +1,16 @@
 const { ChannelType } = require('discord.js');
 const groq = require('./groq');
 
-async function gatherLiveServerConversationsAndSummarize(client, useServerSummarize = false) {
-  const LIVE_GUILD_ID = process.env.GUILD_ID; // pulls from Render
-
-  if (!LIVE_GUILD_ID) throw new Error('GUILD_ID is not defined in environment variables.');
-
-  const guild = client.guilds.cache.get(LIVE_GUILD_ID);
-  if (!guild) throw new Error(`Live server with ID ${LIVE_GUILD_ID} not found.`);
-
+async function gatherServerConversationsAndSummarize(guild, useServerSummarize = false) {
   let allMessages = [];
 
   for (const channel of guild.channels.cache.values()) {
-    if (
-      channel.isTextBased() &&
-      channel.viewable &&
-      !channel.isThread() &&
-      channel.type === ChannelType.GuildText
-    ) {
+    if (channel.isTextBased && channel.viewable && !channel.isThread && channel.type === ChannelType.GuildText) {
       try {
         const messages = await channel.messages.fetch({ limit: 100 });
         const formatted = messages
           .sort((a, b) => a.createdTimestamp - b.createdTimestamp)
-          .map(
-            (msg) =>
-              `[${channel.name}] ${msg.member?.displayName || msg.author.username}: ${msg.content}`
-          );
+          .map((msg) => `[${channel.name}] ${msg.member?.displayName || msg.author.username}: ${msg.content}`);
         allMessages.push(...formatted);
       } catch (err) {
         console.warn(`Could not fetch messages for #${channel.name}:`, err?.message || err);
@@ -43,4 +28,4 @@ async function gatherLiveServerConversationsAndSummarize(client, useServerSummar
   }
 }
 
-module.exports = { gatherLiveServerConversationsAndSummarize };
+module.exports = { gatherServerConversationsAndSummarize };
